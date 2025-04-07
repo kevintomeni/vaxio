bd778b93f8cfc9e9bf45a447df99dca8682e588e
kevintomeni <tomenikevin@icloud.com>
2025-04-07 16:03:01 +0100

    new file

---
+++ lib/Vue/Loading_page/loadingPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Loadingpage extends StatefulWidget {
+  const Loadingpage({super.key});
+
+  @override
+  State<Loadingpage> createState() => _LoadingpageState();
+}
+
+class _LoadingpageState extends State<Loadingpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
